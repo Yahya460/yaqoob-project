cb3c8a2e96fc483d86a1db9db1065121bea15f6a
yahya <yahya9535@gmail.com>
2026-01-17 22:02:12 +0400

    Clean up config.js by removing comments
    Removed Arabic comments and cleaned up config.

--- fakhralanaqah-demo/assets/config.js
+++ fakhralanaqah-demo/assets/config.js
@@ -1,10 +1,6 @@
 // Fakhr Alanaqah - site-wide config (set once, works on all devices)
 window.FA_CONFIG = {
-  // ضع رابط Web App (Apps Script) هنا مرة واحدة ثم ارفع الموقع:
-  // مثال: "https://script.google.com/macros/s/XXXX/exec"
   apiUrl: "https://script.google.com/macros/s/AKfycbwf37uGhSsXHSLpLSEPlC7c0E2X3-a4MjqJ7PHufzXeXKynAGGMamZvqNWng4JwmAup/exec",
-
-  // (اختياري) تلميح للـPIN (لا تكتب الرقم نفسه هنا)
   adminPinHint: "PIN يعقوب",
   staffPinHint: "PIN الموظف"
 };
