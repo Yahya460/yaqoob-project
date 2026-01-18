021a37fa28ab8e199a90bb60e639f1dc96d31a46
yahya <yahya9535@gmail.com>
2026-01-18 08:37:05 +0400

    Update API URL and remove sensitive hints

--- fakhralanaqah-demo/assets/config.js
+++ fakhralanaqah-demo/assets/config.js
@@ -1,7 +1,4 @@
 // Fakhr Alanaqah - site-wide config (set once, works on all devices)
 window.FA_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwf37uGhSsXHSLpLSEPlC7c0E2X3-a4MjqJ7PHufzXeXKynAGGMamZvqNWng4JwmAup/exec",
-  adminPinHint: "PIN يعقوب",
-  staffPinHint: "PIN الموظف"
+  apiUrl: "https://script.google.com/macros/s/XXXXXXXXXXXX/exec"
 };
- 
